b85b917c02c7ed9fdd424be229e3f3f2efe53d24
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-04 13:11:10 -0400

    Added mixed binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MyPropertyBindingComponent } from './my-property-binding/my-property-binding.component';
 import { MyEventBindingComponent } from './my-event-binding/my-event-binding.component';
+import { MyMixedBindingComponent } from './my-mixed-binding/my-mixed-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyPropertyBindingComponent,
-    MyEventBindingComponent
+    MyEventBindingComponent,
+    MyMixedBindingComponent
   ],
   imports: [
     BrowserModule
