e4352d6bf7b97d68831ea1dc0636d50ead5e7ff6
Javier Buitrago <jbuitrago2014@fau.edu>
2017-11-04 11:50:20 -0400

    Extra event binding is working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MyPropertyBindingComponent } from './my-property-binding/my-property-binding.component';
+import { MyEventBindingComponent } from './my-event-binding/my-event-binding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyPropertyBindingComponent
+    MyPropertyBindingComponent,
+    MyEventBindingComponent
   ],
   imports: [
     BrowserModule
